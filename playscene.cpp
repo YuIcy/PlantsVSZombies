3672664eb573a0f3f7673458996ac97b02e179dc
马子豪 <104619817+zhihao2022@users.noreply.github.com>
2022-06-30 18:11:51 +0800

    Update playscene.cpp

--- playscene.cpp
+++ playscene.cpp
@@ -74,10 +74,6 @@ playscene::playscene(QWidget *parent) : QWidget(parent)
                                         {
                                             for(int k=1;k<6;++k)
                                             {
-                                                for(int j=1;j<10;++j)
-                                                {
-                                                    qDebug()<<plthp[j][k];
-                                                }
                                             }
                                             --plthp[zombie[i]->Mx][zombie[i]->My];
                                             if(!zombie[i]->IfEat)
@@ -219,6 +215,23 @@ playscene::playscene(QWidget *parent) : QWidget(parent)
                     }
                     else
                     {
+                        //判断僵尸身前是否有植物
+                        if(zombie[i]->Mx==10)
+                            return;
+                        if(plthp[zombie[i]->Mx][zombie[i]->My]>0)
+                        {
+                            for(int k=1;k<6;++k)
+                            {
+                            }
+                            --plthp[zombie[i]->Mx][zombie[i]->My];
+                            if(!zombie[i]->IfEat)
+                                zombie[i]->Eat(zombie[i]->EatPath);
+                        }
+                        else
+                        {
+                            if(zombie[i]->IfEat)
+                                zombie[i]->Walk(zombie[i]->WalkPath);
+                        }
                         if(zombie[i]->x()<=-100)
                             Zwin();
                     }
